787ab0e4774ae4c2ef20355a3042195ff462ab36
WhiteTrash-crypto <83240923+WhiteTrash-crypto@users.noreply.github.com>
2023-04-04 01:59:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import requests
 import main
 
-payload = {"n" : "" , "M" : "" ,"T" : "","q" : "", "lang" : "ru"}
-urls = ("https://wttr.in/Череповец","https://wttr.in/London","https://wttr.in/SVO")
+article_id = ["Череповец", "London", "SVO"]
+payload = {"n": "", "M": "", "T": "", "q": "", "lang": "ru"}
+
 
 def main():
-  for url in urls:
-    response = requests.get(url, params = payload)
-    response.raise_for_status() 
-    print(response.text)
+    for city in article_id:
+        response = requests.get(f"https://wttr.in/{city}", params=payload)
+        response.raise_for_status()
+        print(response.text)
+
+
 if __name__ == '__main__':
-  main()
+    main()
